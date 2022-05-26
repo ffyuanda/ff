eec14a53268f75b9e76551f9fbbcb0c5a2650d63
Shaoxuan Yuan <shaoxuan.yuan02@gmail.com>
2022-05-26 12:03:48 +0800

    287-find-the-duplicate-number
    Signed-off-by: Shaoxuan Yuan <shaoxuan.yuan02@gmail.com>

---
+++ LeetCode/Misc/287-find-the-duplicate-number.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
+
+#  Floyd's Tortoise and Hare Algorithm
+
+#  Proof:
+#    start-----entry-------
+#                |        |
+#                --------meeting point
+#    x: distance from start to entry
+#    y: distance from entry to meeting point
+#    c: cycle length
+
+#    When tortoise and hare meets at meeting point,
+#      tortoise traveled: x + y,
+#      hare traveled: x + y + n*c (which is x + y + several loops inside the circle)
+
+#    And hare is twice faster than tortoise, so
+#      2(x + y) = x + y + n*c
+
+#    Then we can get
+#      x = n*c - y
+#      x = (n-1)*c + (c-y)
+
+#    Which means
+#      traveling from start to entry (distance is x) and
+#      traveling from meeting point to entry (c-y + several loops in circle)
+#      will finally meet at entry
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        
+        slow = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        return slow
