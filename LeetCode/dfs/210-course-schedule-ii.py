d16145c30b50e4890ce40b18d4eef7d99382cb1d
Shaoxuan Yuan <shaoxuan.yuan02@gmail.com>
2022-08-03 11:02:41 +0800

    210-course-schedule-ii

---
+++ LeetCode/dfs/210-course-schedule-ii.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/course-schedule-ii/
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = [[] for _ in range(numCourses)]
+        visited = [0 for _ in range(numCourses)]
+        self.post_order = []
+        # create graph
+        for pair in prerequisites:
+            x, y = pair
+            graph[x].append(y)
+        # visit each node
+        for i in range(numCourses):
+            if not self.dfs(graph, visited, i):
+                return []
+        return self.post_order
+    
+    # visit course i, mark it as visited, and find cycle along the way
+    def dfs(self, graph, visited, i):
+        # if ith node is marked as being visited, then a cycle is found
+        if visited[i] == -1:
+            return False
+        # if it is done visted, then do not visit again
+        if visited[i] == 1:
+            return True
+        # mark as being visited
+        visited[i] = -1
+        # visit all the neighbours
+        for j in graph[i]:
+            if not self.dfs(graph, visited, j):
+                return False
+        # after visit all the neighbours, mark it as done visited
+        visited[i] = 1
+        
+        # since 
+        self.post_order.append(i)
+        return True
